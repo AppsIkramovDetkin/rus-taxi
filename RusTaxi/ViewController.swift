d1d37373a0d03516dc1fbd6a16e82a0c3fd78302
Ruslan Prozhivin <dearyeah35@gmail.com>
2018-08-21 22:15:40 +0400

    init project

---
+++ RusTaxi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RusTaxi
+//
+//  Created by Ruslan Prozhivin on 21.08.2018.
+//  Copyright © 2018 App's ID. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+

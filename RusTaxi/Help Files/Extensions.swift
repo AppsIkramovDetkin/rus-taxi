b1b0cdb983d2f141f91cc8fdaee70e282016f82a
Ruslan Prozhivin <dearyeah35@gmail.com>
2018-08-24 23:25:49 +0400

    merg master

--- RusTaxi/Help Files/Extensions.swift
+++ RusTaxi/Help Files/Extensions.swift
@@ -26,3 +26,32 @@ extension UIView {
 		}
 	}
 }
+
+
+extension NSLayoutConstraint {
+	static func quadroAspect(on view: UIView) -> NSLayoutConstraint {
+		return NSLayoutConstraint.init(item: view, attribute: .height, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1, constant: 0)
+	}
+	static func contraints(withNewVisualFormat vf: String, dict: [String: Any]) -> [NSLayoutConstraint] {
+		let separatedArray = vf.split(separator: ",")
+		switch separatedArray.count {
+		case 1: return NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[0])", options: [], metrics: nil, views: dict)
+		case 2: return NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[0])", options: [], metrics: nil, views: dict) + NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[1])", options: [], metrics: nil, views: dict)
+		default: return NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[0])", options: [], metrics: nil, views: dict)
+		}
+	}
+}
+
+extension NSLayoutConstraint {
+	static func centerY(for sview: UIView, to view: UIView) -> NSLayoutConstraint {
+		return NSLayoutConstraint.init(item: sview, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0)
+	}
+	
+	static func centerX(for sview: UIView, to view: UIView) -> NSLayoutConstraint {
+		return NSLayoutConstraint.init(item: sview, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
+	}
+	
+	static func set(size: CGSize, for view: UIView) -> [NSLayoutConstraint] {
+		return [NSLayoutConstraint.init(item: view, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: size.height)] + [NSLayoutConstraint.init(item: view, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: size.width)]
+	}
+}

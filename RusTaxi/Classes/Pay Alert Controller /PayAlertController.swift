ba430f8db262234a8ac6dcf3be07bddd2b6f0a6d
Ruslan Prozhivin <dearyeah35@gmail.com>
2018-09-14 20:27:57 +0400

    fix codeStyle

--- RusTaxi/Classes/Pay Alert Controller /PayAlertController.swift
+++ RusTaxi/Classes/Pay Alert Controller /PayAlertController.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 
-class PayAlertController: UIAlertController {
+class PayAlertController {
 	static let shared = PayAlertController()
 	
-	func showPayAlert() {
+	func showPayAlert(in viewController: UIViewController, with completion: @escaping(_ cashText: String, _ cardText: String) -> Void) {
 		let alert = UIAlertController(title: Localize("payAlert"), message: nil, preferredStyle:
 			UIAlertControllerStyle.alert)
 		alert.addTextField(configurationHandler: moneyHandler)
 		alert.addTextField(configurationHandler: cardHandler)
-		alert.addAction(UIAlertAction(title: Localize("cancel"), style: .destructive, handler: nil))
-		self.present(alert, animated: true, completion:nil)
+		alert.addAction(UIAlertAction(title: Localize("cancel"), style: .cancel, handler: nil))
+		viewController.present(alert, animated: true, completion:nil)
 	}
 	
 	private func moneyHandler(textField: UITextField!) {

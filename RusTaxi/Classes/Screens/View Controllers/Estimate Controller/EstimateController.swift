a4bb6a6974f50c7178ab7a82df7f7b231446a133
Ruslan Prozhivin <dearyeah35@gmail.com>
2018-10-11 20:47:07 +0400

    init vc

---
+++ RusTaxi/Classes/Screens/View Controllers/Estimate Controller/EstimateController.swift
@@ -0,0 +1,18 @@
+//
+//  EstimateController.swift
+//  RusTaxi
+//
+//  Created by Ruslan Prozhivin on 11.10.2018.
+//  Copyright © 2018 App's ID. All rights reserved.
+//
+
+import UIKit
+
+class EstimateController: UIViewController {
+	@IBOutlet weak var tableView: UITableView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+}
